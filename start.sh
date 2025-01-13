3a3a050f2576545c9f5a5d985abc68233fed8809
ozip <41739417+ozipoetra@users.noreply.github.com>
2025-01-13 21:22:18 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,29 +1,30 @@
 #!/bin/sh
 
+DIRECTORY="/workspaces/41739417"
+MC_NAME="bedrock-server-1.21.51.02.zip"
+
 if pgrep -x "openvpn" > /dev/null
 then
     echo "openvpn is Running"
 else
-    nohup openvpn --config /workspaces/41739417/bed.ovpn &
+    nohup openvpn --config $DIRECTORY/bed.ovpn &
     echo "Starting openvpn..."
 fi
 
-DIRECTORY="/workspaces/41739417/mc"
-MC_NAME="bedrock-server-1.21.51.02.zip"
-
-if [ ! -d "$DIRECTORY" ]; then
-  mkdir -P $DIRECTORY
-  cd $DIRECTORY
+if [ ! -d "$DIRECTORY/mc" ]; then
+  mkdir -P $DIRECTORY/mc
+  cd $DIRECTORY/mc
   wget --user-agent "linuxwget" https://www.minecraft.net/bedrockdedicatedserver/bin-linux/$MC_NAME
   unzip $MC_NAME
   ln -s ../worlds worlds
+  ln -s ../permissions.json permissions.json
 fi
 
 if pgrep -x "bedrock_server" > /dev/null
 then
     echo "bedrock is Running"
 else
-    cd /workspaces/41739417/mc
+    cd $DIRECTORY/mc
     nohup ./bedrock_server &
     echo "Starting bedrock..."
 fi
