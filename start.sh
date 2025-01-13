2efb7c1a6aa929e4b62a53452eb07af42bfe629a
ozip <41739417+ozipoetra@users.noreply.github.com>
2025-01-13 13:01:27 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
-#!/bin/sh -e
-echo "root:$ROOT_PASSWORD" | chpasswd
-ssh-keygen -A
-exec /usr/sbin/sshd -D -e "$@"
+#!/bin/sh
+if pgrep -x "openvpn" > /dev/null || pgrep -x "bedrock_server" > /dev/null
+then
+    echo "Program is Running"
+else
+    nohup openvpn --config /workspaces/41739417/bed.ovpn &
+    cd /workspaces/41739417/mc
+    nohup ./bedrock_server &
+    echo "starting..."
+fi
+
