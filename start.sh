f800d6f7978f1dc21c5e83ac769a0b5c4dc6f91d
ozip <41739417+ozipoetra@users.noreply.github.com>
2025-05-12 15:00:38 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -37,6 +37,19 @@ else
     echo "Starting bedrock..."
 fi
 
+if command -v "cloudflared" >/dev/null 2>&1; then
+  echo "cloudflared is installed."
+else
+  echo "cloudflare is NOT installed. Begin installing..."
+  # Add cloudflare gpg key
+  mkdir -p --mode=0755 /usr/share/keyrings
+  curl -fsSL https://pkg.cloudflare.com/cloudflare-main.gpg | tee /usr/share/keyrings/cloudflare-main.gpg >/dev/null
+  # Add this repo to your apt repositories
+  echo 'deb [signed-by=/usr/share/keyrings/cloudflare-main.gpg] https://pkg.cloudflare.com/cloudflared any main' | tee /etc/apt/sources.list.d/cloudflared.list
+  # install cloudflared
+  apt-get update && apt-get install cloudflared
+fi
+
 while true
 do
   sleep 1800
